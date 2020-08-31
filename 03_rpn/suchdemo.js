38f6d92d0443176a6e14837a4a0c060ee6ddf9d6
hschellinger <h.schellinger@posteo.de>
2020-08-31 16:39:00 +0200

    aufgaben teilweise fertig

---
+++ 03_rpn/suchdemo.js
@@ -0,0 +1,7 @@
+const suche = 'https://swc.ff-labs.ml/fact-finder/Search.ff?channel=bergfreunde-co-uk&format=json&query=';
+
+const suggestAPI = 'https://swc.ff-labs.ml/fact-finder/Suggest.ff?channel=bergfreunde-co-uk&format=json&query=';
+
+let produkte = {};
+
+let suggestiopns = {};
